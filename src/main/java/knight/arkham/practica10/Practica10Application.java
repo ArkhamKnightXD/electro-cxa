e2f7fa7208282a1fb8f4cf086d7e267730db9d25
kevin <karvin_jimenez@hotmail.com>
2021-05-02 00:09:14 -0400

    extra changes

--- src/main/java/knight/arkham/practica10/Practica10Application.java
+++ src/main/java/knight/arkham/practica10/Practica10Application.java
@@ -2,12 +2,9 @@ package knight.arkham.practica10;
 
 import knight.arkham.practica10.controladores.ClienteController;
 import knight.arkham.practica10.controladores.EquipoController;
-import knight.arkham.practica10.servicios.SeguridadServices;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
-
 import java.io.File;
 
 @SpringBootApplication
@@ -19,36 +16,13 @@ public class Practica10Application {
 
 	public static void main(String[] args) {
 
-
 		//Esto creara el direcctorio especificado en el controlador al inicio
 		// del programa si no existe, tanto para cliente como para equipo
 		new File(ClienteController.uploadDirectory).mkdir();
 
 		new File(EquipoController.uploadDirectory).mkdir();
 
-
 		SpringApplication.run(Practica10Application.class, args);
 
-
-		// Por ahora la creacion del usuario admin me sigue fallando, pero me funciona de otra forma
-		//guardare este codigo aqui por si luego hace falta
-
-		// Esta funcion es necesario para obtener los distintos bean y tambien para poder llamar usuario services al main
-		/*ApplicationContext applicationContext = SpringApplication.run(Practica10Application.class, args);
-		String[] lista = applicationContext.getBeanDefinitionNames();
-		System.out.println("====== Beans Registrados =====");
-		for(String bean : lista){
-			System.out.println(""+bean);
-		}
-		System.out.println("====== FIN Beans Registrados =====");
-
-		// Aqui prepara el usuario service para que pueda ser utilizado aqui
-		SeguridadServices seguridadServices = (SeguridadServices) applicationContext.getBean("seguridadServices");
-
-		// y aqui finalmente creo el usuario admin
-		seguridadServices.crearUsuarioAdmin();
-*/
-
 	}
-
 }
